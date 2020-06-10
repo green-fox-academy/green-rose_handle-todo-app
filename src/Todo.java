ff38fe20fe6ad43ad92d82b31b5930d2dfbebcf8
Mila Karous <m.k.rose@seznam.cz>
2020-06-10 12:29:52 +0200

    input argument logic added

--- src/Todo.java
+++ src/Todo.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Todo {
     public static void main(String[] args) {
-
+        List<String> allowedArguments = new ArrayList<String>();
+        allowedArguments.add("-l");
         if (args.length==0) PrintUsage.printUsage();
-        Scanner s = new Scanner(System.in);
-        String question = s.nextLine();
+        else if (allowedArguments.contains(args[0])) {
+            // Todo Some Logic here
+        } else {
+            System.out.println("Cant recognize your argument: "+args[0]);
+            PrintUsage.printUsage();
+        }
     }
 }
